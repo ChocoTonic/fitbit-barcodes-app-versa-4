20bca74776354c531f6d5f1a75ff8bfa15946c86
ChocoTonic <nutspizza@gmail.com>
2026-02-03 15:11:42 -0800

    apply consistent formatting

--- companion/index.js
+++ companion/index.js
@@ -1,71 +1,70 @@
-import {outbox} from "file-transfer";
-import {settingsStorage as store} from "settings";
-import {peerSocket} from "messaging";
-import {me} from "companion";
+import { me } from "companion";
+import { outbox } from "file-transfer";
+import { peerSocket } from "messaging";
+import { settingsStorage as store } from "settings";
 
 var cards = [];
 var bright = false;
 
 function trim(s) {
-  return (s.charAt && s.charAt(0) === '"') ? s.substr(1, s.length - 2) : s;
+    return s.charAt && s.charAt(0) === '"' ? s.substr(1, s.length - 2) : s;
 }
 
-peerSocket.onmessage = e => {
-  if(e.data && e.data.getAll) sendAll();
+peerSocket.onmessage = (e) => {
+    if (e.data && e.data.getAll) sendAll();
 };
 
-store.onchange = e => {
-  setCard(e.key, e.newValue);
-  sendAll();
+store.onchange = (e) => {
+    setCard(e.key, e.newValue);
+    sendAll();
 };
 
 function setCard(name, value) {
-  let i = name.substr(-1);  //e.g., code1,color1
-  if(/^\d+$/.test(i)) {
-    i = i*1 - 1;
-    if(!cards[i]) cards[i] = {};
-    name = name.substr(0, name.length - 1);
+    let i = name.substr(-1); //e.g., code1,color1
+    if (/^\d+$/.test(i)) {
+        i = i * 1 - 1;
+        if (!cards[i]) cards[i] = {};
+        name = name.substr(0, name.length - 1);
 
-    if(name === "name" || name === "code") {
-      value = (JSON.parse(value).name || "").replace(/^\s+|\s+$/g, "");
-    } else if(name === "color") {
-      value = trim(value);
-    } else if(name === "type") {
-      value = JSON.parse(value).selected[0];
+        if (name === "name" || name === "code") {
+            value = (JSON.parse(value).name || "").replace(/^\s+|\s+$/g, "");
+        } else if (name === "color") {
+            value = trim(value);
+        } else if (name === "type") {
+            value = JSON.parse(value).selected[0];
+        }
+        cards[i][name] = value;
+    } else if (name === "bright") {
+        bright = value === "true";
     }
-    cards[i][name] = value;
-
-  } else if(name === "bright") {
-    bright = (value === "true");
-  }
 }
 
 function init() {
-  for(let i = store.length - 1; i >= 0; i--) {
-    let k = store.key(i);
-    setCard(k, store.getItem(k));
-  }
+    for (let i = store.length - 1; i >= 0; i--) {
+        let k = store.key(i);
+        setCard(k, store.getItem(k));
+    }
 
-  if(me.launchReasons.settingsChanged) sendAll();
+    if (me.launchReasons.settingsChanged) sendAll();
 }
 
 init();
 
 function sendAll() {
-  let tmp = [];
-  for(let i = 0; i < cards.length; i++) {
-    if(cards[i] && cards[i].code) {
-      let o = {code: cards[i].code};
-      if(cards[i].name) o.name = encodeURIComponent(cards[i].name);
-      if(cards[i].color) o.color = cards[i].color;
-      if(cards[i].type) o.type = cards[i].type;
-      tmp.push(o);
+    let tmp = [];
+    for (let i = 0; i < cards.length; i++) {
+        if (cards[i] && cards[i].code) {
+            let o = { code: cards[i].code };
+            if (cards[i].name) o.name = encodeURIComponent(cards[i].name);
+            if (cards[i].color) o.color = cards[i].color;
+            if (cards[i].type) o.type = cards[i].type;
+            tmp.push(o);
+        }
     }
-  }
-  tmp = JSON.stringify({cards: tmp, bright: bright});
-  let data = new Uint8Array(tmp.length);
-  for (let i = 0; i < data.length; i++) {
-    data[i] = tmp.charCodeAt(i);
-  }
-  outbox.enqueue("settings.txt", data);
+    tmp = JSON.stringify({ cards: tmp, bright: bright });
+    let data = new Uint8Array(tmp.length);
+    for (let i = 0; i < data.length; i++) {
+        data[i] = tmp.charCodeAt(i);
+    }
+    outbox.enqueue("settings.txt", data);
 }
--- settings/index.jsx
+++ settings/index.jsx
@@ -1,122 +1,225 @@
 var allColors = [
-  {color: "#FF4949"},  //red
-  {color: "#FEB300"},  //orange
-  {color: "#DAD700"},  //yellow
-  {color: "#25FF86"},  //lime
-  {color: "#12D612"},  //green
-  {color: "#6FD4ED"},  //light blue
-  {color: "#535BFF"},  //blue
-  {color: "#FF40FF"},  //purple
-  {color: "#C6643E"},  //brown
-  {color: "#808080"}  //grey
+    { color: "#FF4949" }, //red
+    { color: "#FEB300" }, //orange
+    { color: "#DAD700" }, //yellow
+    { color: "#25FF86" }, //lime
+    { color: "#12D612" }, //green
+    { color: "#6FD4ED" }, //light blue
+    { color: "#535BFF" }, //blue
+    { color: "#FF40FF" }, //purple
+    { color: "#C6643E" }, //brown
+    { color: "#808080" }, //grey
 ];
 
 var allTypes = [
-  {name: "EAN/UPC/Code-128", value: "0"},
-  {name: "Code-128", value: "1"},
-  {name: "Code-39", value: "2"}
+    { name: "EAN/UPC/Code-128", value: "0" },
+    { name: "Code-128", value: "1" },
+    { name: "Code-39", value: "2" },
 ];
 
 function showWarning(str) {
-  try {
-    if(!JSON.parse(str).name) return null;
-  } catch(e) {
-    return null;
-  }
-  return (
-    <Section title={<Text>Tip: What does &quot;<Text bold>Code too long</Text>&quot; mean?</Text>}>
-      <Text>The more characters your barcode has, the more horizontal space is required to display the barcode. When the
-      required barcode space exceeds the physical screen space of your device, you will get a &quot;<Text italic>Code too long</Text>&quot; 
-      message, because the barcode cannot fit on the screen.</Text>
-    </Section>
-    )
+    try {
+        if (!JSON.parse(str).name) return null;
+    } catch (e) {
+        return null;
+    }
+    return (
+        <Section
+            title={
+                <Text>
+                    Tip: What does &quot;<Text bold>Code too long</Text>&quot;
+                    mean?
+                </Text>
+            }
+        >
+            <Text>
+                The more characters your barcode has, the more horizontal space
+                is required to display the barcode. When the required barcode
+                space exceeds the physical screen space of your device, you will
+                get a &quot;<Text italic>Code too long</Text>&quot; message,
+                because the barcode cannot fit on the screen.
+            </Text>
+        </Section>
+    );
 }
 
 function trim(s) {
-  return s && s.replace(/^\s+|\s+$/g, "");
+    return s && s.replace(/^\s+|\s+$/g, "");
 }
 
 function prettyAgo(t) {
-  if(t) {
-    t = Date.now()/1000 - t;
-    if(t < 60) {
-      return "Saved now!"
-    } else if(t < 60*60) {
-      return "Saved " + Math.floor(t/60) + " minute(s) ago";
-    } else if(t < 24*60*60) {
-      return "Saved " + Math.floor(t/(60*60)) + " hour(s) ago";
+    if (t) {
+        t = Date.now() / 1000 - t;
+        if (t < 60) {
+            return "Saved now!";
+        } else if (t < 60 * 60) {
+            return "Saved " + Math.floor(t / 60) + " minute(s) ago";
+        } else if (t < 24 * 60 * 60) {
+            return "Saved " + Math.floor(t / (60 * 60)) + " hour(s) ago";
+        }
     }
-  }
-  return "";
+    return "";
 }
 
 function toObj(json) {
-  if(!json) return {};
-  try {
-    return JSON.parse(json);
-  } catch(e) {
-    return {};
-  }
+    if (!json) return {};
+    try {
+        return JSON.parse(json);
+    } catch (e) {
+        return {};
+    }
 }
 
-registerSettingsPage(props => {  
-  return (
-    <Page>
-      {showWarning(props.settings.code1)}
-      <Toggle settingsKey="bright" label="Increase screen brightness" />
+registerSettingsPage((props) => {
+    return (
+        <Page>
+            {showWarning(props.settings.code1)}
+            <Toggle settingsKey="bright" label="Increase screen brightness" />
 
-      <Section title="Barcode 1">
-        <TextInput settingsKey="name1" title="Name" placeholder="e.g., 7-Eleven" />
-        <TextInput settingsKey="code1" label="Barcode" placeholder="e.g., 12345678" />
-        <ColorSelect settingsKey="color1" colors={allColors} />
-        <Select settingsKey="type1" label="Encoding" options={allTypes} />
-      </Section>
+            <Section title="Barcode 1">
+                <TextInput
+                    settingsKey="name1"
+                    title="Name"
+                    placeholder="e.g., 7-Eleven"
+                />
+                <TextInput
+                    settingsKey="code1"
+                    label="Barcode"
+                    placeholder="e.g., 12345678"
+                />
+                <ColorSelect settingsKey="color1" colors={allColors} />
+                <Select
+                    settingsKey="type1"
+                    label="Encoding"
+                    options={allTypes}
+                />
+            </Section>
 
-      <Section title="Barcode 2">
-        <TextInput settingsKey="name2" title="Name"  placeholder="e.g., 7-Eleven" />
-        <TextInput settingsKey="code2" title="Barcode" placeholder="e.g., 12345678" />
-        <ColorSelect settingsKey="color2" colors={allColors} />
-        <Select settingsKey="type2" label="Encoding" options={allTypes} />
-      </Section>
+            <Section title="Barcode 2">
+                <TextInput
+                    settingsKey="name2"
+                    title="Name"
+                    placeholder="e.g., 7-Eleven"
+                />
+                <TextInput
+                    settingsKey="code2"
+                    title="Barcode"
+                    placeholder="e.g., 12345678"
+                />
+                <ColorSelect settingsKey="color2" colors={allColors} />
+                <Select
+                    settingsKey="type2"
+                    label="Encoding"
+                    options={allTypes}
+                />
+            </Section>
 
-      <Section title="Barcode 3">
-        <TextInput settingsKey="name3" title="Name"  placeholder="e.g., 7-Eleven" />
-        <TextInput settingsKey="code3" title="Barcode" placeholder="e.g., 12345678" />
-        <ColorSelect settingsKey="color3" colors={allColors} />
-        <Select settingsKey="type3" label="Encoding" options={allTypes} />
-      </Section>
+            <Section title="Barcode 3">
+                <TextInput
+                    settingsKey="name3"
+                    title="Name"
+                    placeholder="e.g., 7-Eleven"
+                />
+                <TextInput
+                    settingsKey="code3"
+                    title="Barcode"
+                    placeholder="e.g., 12345678"
+                />
+                <ColorSelect settingsKey="color3" colors={allColors} />
+                <Select
+                    settingsKey="type3"
+                    label="Encoding"
+                    options={allTypes}
+                />
+            </Section>
 
-      <Section title="Barcode 4">
-        <TextInput settingsKey="name4" title="Name"  placeholder="e.g., 7-Eleven" />
-        <TextInput settingsKey="code4" title="Barcode" placeholder="e.g., 12345678" />
-        <ColorSelect settingsKey="color4" colors={allColors} />
-        <Select settingsKey="type4" label="Encoding" options={allTypes} />
-      </Section>
+            <Section title="Barcode 4">
+                <TextInput
+                    settingsKey="name4"
+                    title="Name"
+                    placeholder="e.g., 7-Eleven"
+                />
+                <TextInput
+                    settingsKey="code4"
+                    title="Barcode"
+                    placeholder="e.g., 12345678"
+                />
+                <ColorSelect settingsKey="color4" colors={allColors} />
+                <Select
+                    settingsKey="type4"
+                    label="Encoding"
+                    options={allTypes}
+                />
+            </Section>
 
-      <Section title="Barcode 5">
-        <TextInput settingsKey="name5" title="Name"  placeholder="e.g., 7-Eleven" />
-        <TextInput settingsKey="code5" title="Barcode" placeholder="e.g., 12345678" />
-        <ColorSelect settingsKey="color5" colors={allColors} />
-        <Select settingsKey="type5" label="Encoding" options={allTypes} />
-      </Section>
+            <Section title="Barcode 5">
+                <TextInput
+                    settingsKey="name5"
+                    title="Name"
+                    placeholder="e.g., 7-Eleven"
+                />
+                <TextInput
+                    settingsKey="code5"
+                    title="Barcode"
+                    placeholder="e.g., 12345678"
+                />
+                <ColorSelect settingsKey="color5" colors={allColors} />
+                <Select
+                    settingsKey="type5"
+                    label="Encoding"
+                    options={allTypes}
+                />
+            </Section>
 
-      <Section title="Barcode 6">
-        <TextInput settingsKey="name6" title="Name"  placeholder="e.g., 7-Eleven" />
-        <TextInput settingsKey="code6" title="Barcode" placeholder="e.g., 12345678" />
-        <ColorSelect settingsKey="color6" colors={allColors} />
-        <Select settingsKey="type6" label="Encoding" options={allTypes} />
-      </Section>
+            <Section title="Barcode 6">
+                <TextInput
+                    settingsKey="name6"
+                    title="Name"
+                    placeholder="e.g., 7-Eleven"
+                />
+                <TextInput
+                    settingsKey="code6"
+                    title="Barcode"
+                    placeholder="e.g., 12345678"
+                />
+                <ColorSelect settingsKey="color6" colors={allColors} />
+                <Select
+                    settingsKey="type6"
+                    label="Encoding"
+                    options={allTypes}
+                />
+            </Section>
 
-      <Section title="Barcode 7">
-        <TextInput settingsKey="name7" title="Name"  placeholder="e.g., 7-Eleven" />
-        <TextInput settingsKey="code7" title="Barcode" placeholder="e.g., 12345678" />
-        <ColorSelect settingsKey="color7" colors={allColors} />
-        <Select settingsKey="type7" label="Encoding" options={allTypes} />
-      </Section>
-      
-      <Section description={prettyAgo(props.settings.clickButton)}>
-        <Button label="Save" onClick={() => props.settingsStorage.setItem("clickButton", ""+Math.floor(Date.now()/1000)) } />
-      </Section>
-    </Page>
-  );
+            <Section title="Barcode 7">
+                <TextInput
+                    settingsKey="name7"
+                    title="Name"
+                    placeholder="e.g., 7-Eleven"
+                />
+                <TextInput
+                    settingsKey="code7"
+                    title="Barcode"
+                    placeholder="e.g., 12345678"
+                />
+                <ColorSelect settingsKey="color7" colors={allColors} />
+                <Select
+                    settingsKey="type7"
+                    label="Encoding"
+                    options={allTypes}
+                />
+            </Section>
+
+            <Section description={prettyAgo(props.settings.clickButton)}>
+                <Button
+                    label="Save"
+                    onClick={() =>
+                        props.settingsStorage.setItem(
+                            "clickButton",
+                            "" + Math.floor(Date.now() / 1000),
+                        )
+                    }
+                />
+            </Section>
+        </Page>
+    );
 });
